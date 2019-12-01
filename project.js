14abe0b014ebd3b6617e63f0aacdff86e9208e10
aladdinomar <aladdinomar@aladdins-air.home>
2019-12-01 15:44:58 -0500

    achieved MVP

--- project.js
+++ project.js
@@ -1,102 +1,135 @@
-//button I am accessing from HTML
-playButton = document.querySelector('button');
-//total score
-let score = 0;
 
-// when you press this button, the playRound function starts
-playButton.addEventListener('click', async function () {
-    playRound();
+let playButton = document.querySelector('button');
+let cardImg = document.createElement('img');
+document.body.appendChild(cardImg);
+
+let choice1 = document.querySelector('#high');
+let choice2 = document.querySelector('#low');
+let choice3 = document.querySelector('#same');
+
+let userChoice = null;
+
+let deck = null;
+let scoreBoard = document.querySelector('.score-count');
+let score = scoreBoard.innerHTML;
+
+let currCard = null;
+let nextCard = null;
+
+choice1.addEventListener('click', function () {
+    makeChoice(-1);
+});
+choice2.addEventListener('click', function () {
+    makeChoice(1);
+});
+choice3.addEventListener('click', function () {
+    makeChoice(0);
 });
 
-//pulls up a card from an array of cards, makes a loop through all the cards, 
-async function playRound() {
-    let deck = await axios.get('https://deckofcardsapi.com/api/deck/new/draw/?count=1');
-    deckID = deck.data.deck_id
-    let card = await axios.get(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=52`)
+playButton.addEventListener('click', async function () {
+    // TODO: Disable play after first click
+    deck = await createDeck();
+    currCard = deck.pop();
+    startRound();
+});
 
-    let cardVal = card.data.cards
-    let deckVal = [];
-    //cardVal[i].value/suit
-
-
-    //this loop checks if the value of the card is either KING QUEEN JACK OR ACE and changes them to a number value, otherwise, everything else is pushed into a new array because the current one its in is immutable.
-    for (let i = 0; i < cardVal.length; i++) {
-        if (cardVal[i].value === "KING") {
-            deckVal.push(13);
-        } else if (cardVal[i].value === "QUEEN") {
-            deckVal.push(12);
-        } else if (cardVal[i].value === "JACK") {
-            deckVal.push(11);
-        } else if (cardVal[i].value === "ACE") {
-            deckVal.push(1);
+async function createDeck() {
+    // let deck = await axios.get('https://deckofcardsapi.com/api/deck/new/draw/?count=0');
+    // deckID = deck.data.deck_id
+    let cards = await axios.get(`https://deckofcardsapi.com/api/deck/new/draw/?count=52`);
+    cardObj = cards.data.cards;
+    // console.log(cards.data.cards[0].images.svg);
+    // console.log(cardObj.length);
+    // console.log(cardObj)
+    for (let i = 0; i < cardObj.length; i++) {
+        if (cardObj[i].value === 'KING') {
+            cardObj[i].value = 13;
+        } else if (cardObj[i].value === 'QUEEN') {
+            cardObj[i].value = 12;
+        } else if (cardObj[i].value === 'JACK') {
+            cardObj[i].value = 11;
+        } else if (cardObj[i].value === 'ACE') {
+            cardObj[i].value = 1;
         } else {
-            deckVal.push(Number(cardVal[i].value));
+            cardObj[i].value = Number(cardObj[i].value)
         }
     }
-
-    for (let i = 0; i < deckVal.length; i++){
-        let card1 = deckVal[i];
-        let card2 = deckVal[i+1];
-        console.log(card1);
-        console.log(cardVal[0].suit);
-        if (checkBet(card1, card2)) {
-            alert('Nice! You now have ' + score + ' points')
-        } else {
-            alert('Nope! Score is now ' + score);
-        }
-    }
-    gameEnd();
-
-    // let card1 = deckVal[0];
-    // let card2 = deckVal[1];
-    // // console.log(deckVal)
-    // console.log(card1);
-    // console.log(cardVal[0].suit);
-    // checkBet(card1, card2);
-    
-    // console.log(card2);
-    // console.log(cardVal[1].suit);
-    // checkBet(card1, card2)
+    return cardObj;
 }
+
+async function renderCards(card) {
+    // for (i = 0; i < deck.length; i++) {
+    let pic = card.images.svg;
+    // console.log("image");
+    cardImg.setAttribute("src", pic);
+    cardImg.setAttribute("width", "200");
+    cardImg.setAttribute("height", "300");
+    // console.log("end");
+    // }
+}
+// let card1 = deckVal[i];
+// let card2 = deckVal[i + 1];
+// console.log(card1);
+// console.log(cardVal[0].suit);
+// // makeGuess(card1, card2)
+// makeChoice(card1, card2)
+
+// renderCards();
 
 function compare(card1, card2) {
-    return card1 - card2;
-
+    return card1.value - card2.value;
 }
 
-function makeGuess(card1, card2) {
-    let bet = (window.prompt(`Will the next card be higher(h), lower(l), or the same(s)`));
-    switch (bet) {
-        case 'h':
-            return compare(card1, card2) < 0;
-        case 'l':
-            return compare(card1, card2) > 0;
-        case 's':
-            return compare(card1, card2) === 0;
-        default:
-            alert('Pick from the choices I gave you!');
-            return false;
-    }
-}
-
-function checkBet(card1, card2) {
-    let guess = makeGuess(card1, card2);
-    if (guess === true) {
+function makeChoice(option) {
+    // will the next card be higher or lower than current card?
+    userChoice = option;
+    // let currScore = 0;
+    if (option === correctChoice(currCard, nextCard)) {
         score++;
-        return true;
-        // alert('Nice! You now have ' + score + ' points')
-    } else if (guess === false) {
-        score--;
-        return false;
-        // alert('Nope! Score is now ' + score)
-    }
-}
-
-
-function gameEnd() {
-    if (score >= 23) {
-        alert('Congrats, you won! Thanks for playing')
+        console.log(score);
+        scoreBoard.innerHTML = score;
+        console.log("Right!");
+    } else if (score === 0) {
+        score -= 0;
+        scoreBoard.innerHTML = score;
+        console.log('zero points, no subtraction')
     } else {
-        alert('You lose! Try again?')
+        score--;
+        scoreBoard.innerHTML = score;
+        console.log("Wrong!");
+    }
+    currCard = nextCard;
+    userChoice = null;
+    startRound();
+}
+
+function correctChoice(card1, card2) {
+    let val = compare(card1, card2);
+    if (val > 0) {
+        return 1;
+    } else if (val === 0) {
+        return 0;
+    } else if (val < 0) {
+        return -1;
     }
 }
+
+
+function startRound() {
+    if (deck.length === 0) {
+        endGame();
+    }
+    else {
+        renderCards(currCard);
+        nextCard = deck.pop();
+    }
+
+    function endGame() {
+        if (score >= 23) {
+            alert('You son of a BET-CH, you won! Thanks for playing')
+        } else {
+            alert('You lost! Better luck next time!')
+        }
+    }
+}
+
